4eac470bd58b3ddaba29bad0e8e5b894159ccf26
Jeremy Muhia <jmuhia80@gmail.com>
2016-11-05 10:45:34 -0400

    minor updates

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
 import os
 import urlparse
 import psycopg2
