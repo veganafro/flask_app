80cf188fba4bdf01a06dcaf0b1ecaab2527eaf82
veganafro <jmuhia80@gmail.com>
2017-01-04 22:02:58 -0800

    build templates

--- flask_app.py
+++ flask_app.py
@@ -73,7 +73,7 @@ def contact():
             # do something using maybe a self built api to send an email
             return redirect('/contact'), 200
         elif request.method == 'GET':
-            return render_template('contact.hmtl'), 200
+            return render_template('contact.hmtl', social_data=social_data), 200
     except:
         return redirect('/error'), 404
 
@@ -87,13 +87,13 @@ def social():
 @app.route('/about')
 def about():
     try:
-        return render_template('about.html'), 200
+        return render_template('about.html', social_data=social_data), 200
     except:
         return redirect('/error'), 404
 
 @app.route('/error')
 def error():
-    return render_template('error.html'), 404
+    return render_template('error.html', social_data=social_data), 404
 
 
 if __name__ == '__main__':
